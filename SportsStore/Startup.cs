8da97607fdd0a0f59d3275c739b34c44ff010efc
Ildar Tagiev <tag.ildar@gmail.com>
2021-05-21 21:33:36 +0300

    preparing blazor server, managing orders

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -40,6 +40,7 @@ namespace SportsStore
 			services.AddSession();
 			services.AddScoped<CartModel>(sp => SessionCart.GetCart(sp));
 			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddServerSideBlazor();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,6 +76,8 @@ namespace SportsStore
 
 				endpoints.MapDefaultControllerRoute();
 				endpoints.MapRazorPages();
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 			});
 
 			SeedData.EnsurePopulated(app);
