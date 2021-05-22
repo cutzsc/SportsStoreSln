9adc9de6f4771debf5fe5c51b59e237e3ee6c511
Ildar Tagiev <tag.ildar@gmail.com>
2021-05-22 11:52:27 +0300

    Catalog Managment CRUD Products

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -13,5 +13,22 @@ namespace SportsStore.Models
 			this.context = context;
 
 		public IQueryable<Product> Products => context.Products;
+
+		public void CreateProduct(Product product)
+		{
+			context.Add(product);
+			context.SaveChanges();
+		}
+
+		public void DeleteProduct(Product product)
+		{
+			context.Remove(product);
+			context.SaveChanges();
+		}
+
+		public void SaveProduct(Product product)
+		{
+			context.SaveChanges();
+		}
 	}
 }
